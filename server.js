5a421e6757faafe5b965a8949a921eb87c6d0e7e
Richard Clarke <freshnewrich@gmail.com>
2022-04-05 22:09:38 -0700

    basic setup, files, installs

---
+++ server.js
@@ -0,0 +1,16 @@
+require("dotenv").config()
+const PORT = process.env.PORT || 3001
+
+const express = require('express')
+const app = express()
+
+const drinks = require('./models/drinks.js')
+
+app.listen(PORT, () => {
+    console.log(`Listening port: ${PORT}`)
+})
+
+app.get("/", (req,res) => {
+    res.send('Welcome to the Gitpub App!')
+})
+
