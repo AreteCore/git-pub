3d9f35ae65042da3eb54ffadf89bd1cc64b46ca2
Richard Clarke <freshnewrich@gmail.com>
2022-04-08 16:58:11 -0700

    kinda done

--- server.js
+++ server.js
@@ -5,13 +5,22 @@ const express = require('express')
 const app = express()
 
 const drinks = require('./models/drinks.js')
+const foods = require('./models/food.js')
 
 app.listen(PORT, () => {
     console.log(`Listening port: ${PORT}`)
 })
 
 app.get("/", (req,res) => {
-    res.render('drinks_index.ejs',{drinksMenu: drinks})
+    res.render('drinks_index.ejs',{
+        drinksMenu: drinks,
+    })
+})
+
+app.get("/food", (req,res) => {
+    res.render('food_index.ejs',{
+        foodMenu: foods,
+    })
 })
 
 app.get("/drinks/:id", (req,res) => {
@@ -20,6 +29,11 @@ app.get("/drinks/:id", (req,res) => {
     })
 })
 
+app.get("/food/:id", (req,res) => {
+    res.render('food_show.ejs', {
+        food: foods[req.params.id]
+    })
+})
 
 // next step
 //https://seir-222-sasquatch.netlify.app/backend-fundamentals/week-7/day-1/labs/git-pub/#rendering-an-individual-drink-in-the-show-view
